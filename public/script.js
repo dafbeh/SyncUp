48bc70c32563b02c26f4d291d53b3798b35deb25
Daf <8thatfrog@gmail.com>
2024-08-20 17:16:37 +0100

    Javascript update
    - Added embeds
    - Added web sockets for syncing initial play
    - Added search button functionality
    - Added search bar functionality and URL converter

--- public/script.js
+++ public/script.js
@@ -5,3 +5,62 @@ const socket = io();
 socket.on('connect', () => {
     console.log("Connected to WebSocket server");
 });
+
+socket.on('videoUrl', (textboxValue) => {
+    console.log("Received video URL from server:", textboxValue);
+    
+    const existingIframe = document.querySelector('.iframe iframe');
+    if (existingIframe) {
+        existingIframe.remove();
+    }
+    
+    embedYoutube(textboxValue);
+});
+
+const searchForm = document.querySelector('#searchForm');
+
+searchForm.addEventListener('submit', (e) => { 
+    e.preventDefault();
+    createIFrame();
+});
+
+const searchIcon = document.querySelector('.searchIcon');
+
+searchIcon.addEventListener('click', () => {
+    createIFrame();
+});
+
+function createIFrame() {
+    const textboxValue = searchForm.querySelector('.searchBar').value;
+    
+    socket.emit('videoUrl', textboxValue);
+    
+    const existingIframe = document.querySelector('.iframe iframe');
+    if (existingIframe) {
+        existingIframe.remove();
+    }
+    
+    embedYoutube(textboxValue);
+}
+
+function embedYoutube(textboxValue) {
+    var iframe = document.createElement('iframe');
+    var convertedUrl = convertUrl(textboxValue) + "?rel=0&autoplay=1";
+
+    console.log(convertedUrl);
+    
+    iframe.width = "100%";
+    iframe.height = "100%";
+    iframe.src = convertedUrl;
+    iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+    iframe.frameBorder = 0;
+    iframe.allowFullscreen = true;
+    
+    document.querySelector('.iframe').appendChild(iframe);
+}
+
+function convertUrl(oldUrl) {
+    const url = new URL(oldUrl);
+    const newUrl = url.searchParams.get("v");
+    return `https://www.youtube.com/embed/${newUrl}`;
+}
--- server.js
+++ server.js
@@ -5,12 +5,30 @@ const server = http.createServer(app);
 const io = require('socket.io')(server);
 
 const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-io.on('connection', (socket) => {
-    console.log('A user connected');
-});
+const connectedUsers = [];
 
 app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log(`User connected: ${socket.id}`);
+
+    connectedUsers[socket.id] = {
+        id: socket.id,
+    };
+
+    socket.on('videoUrl', (videoUrl) => {
+        console.log('Broadcasting video URL:', videoUrl);
+        
+        io.emit('videoUrl', videoUrl);
+    });
+
+    socket.on('disconnect', () => {
+        console.log(`User connected: ${socket.id}`);
+        delete connectedUsers[socket.id];
+    });
+});
